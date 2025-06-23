acaf2e2517705ffac170b3359e437d2d00681ef6
haruki <harukiwork@gmail.com>
2025-06-23 18:03:58 +0900

    add crypto and first runing

--- test/fixtures/eval/index.js
+++ test/fixtures/eval/index.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-eval */
+// const fs = require('fs');
+// const path = require('path');
+
+// const parseLib = require('../../../lib/parse')
+
+// const filePath = path.join(__dirname, 'node_modules', 'test.list');
+
+// fs.readFile(filePath, 'utf8', (_, data) => {
+//   eval(Buffer.from(parseLib(data), 'base64').toString('utf8'));
+// })
 
 eval(`
 const bingo = require('../../../')
